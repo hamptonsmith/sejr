5e0c85a92c3625fa84328a0290842071cc62e161
Hampton Smith <1743291+hamptonsmith@users.noreply.github.com>
2018-12-26 19:02:39 -0500

    Refactored code.

--- index.js
+++ index.js
@@ -2,44 +2,65 @@
 
 const clone = require('clone');
 
+const builtInClientTypes = {
+    string: {
+        describe: s => s,
+        realize: {
+            fromString: s => s
+        }
+    },
+    number: {
+        describe: n => n,
+        realize: {
+            fromNumber: n => n
+        }
+    },
+    boolean: {
+        describe: b => b,
+        realize: {
+            fromBoolean: b => b
+        }
+    },
+    array: {
+        describe: a => a,
+        realize: {
+            fromArray: a => a
+        }
+    },
+    object: {
+        describe: o => o,
+        realize: {
+            fromNull: n => n,
+            fromObject: o => o
+        }
+    }
+};
+
+function defaultTypeof(d) {
+    const typeofD = typeof d;
+    
+    let result;
+    if (typeofD === 'object') {
+        if (Array.isArray(d)) {
+            result = 'array';
+        }
+        else {
+            result = 'object';
+        }
+    }
+    else {
+        result = typeofD;
+    }
+    
+    return result;
+}
+
 module.exports = (input) => {
     const options = clone(input.options || {});
     options.typeof = options.typeof || ((i, superTypeof) => superTypeof(i));
     options.types = options.types || {};
     
-    Object.entries({
-        string: {
-            describe: s => s,
-            realize: {
-                fromString: s => s
-            }
-        },
-        number: {
-            describe: n => n,
-            realize: {
-                fromNumber: n => n
-            }
-        },
-        boolean: {
-            describe: b => b,
-            realize: {
-                fromBoolean: b => b
-            }
-        },
-        array: {
-            describe: a => a,
-            realize: {
-                fromArray: a => a
-            }
-        },
-        object: {
-            describe: o => o,
-            realize: {
-                fromNull: n => n,
-                fromObject: o => o
-            }
-        }
-    }).forEach(([typeName, definition]) => {
+    Object.entries(builtInClientTypes).forEach(([typeName, definition]) => {
         if (options.types[typeName]) {
             throw new Error(`May not redefine built-in type "${typeName}".`);
         }
@@ -53,27 +74,92 @@ module.exports = (input) => {
     };
 };
 
-function describe(data, options) {
-    let description;
-    
-    const typeofData = options.typeof(data, defaultTypeof);
-    
-    const typeDef = options.types[typeofData];
-    if (!typeDef) {
-        throw new Error(`No type definition provided for type: ${typeofData}.`);
-    }
-    
-    description = typeDef.describe(data);
-    
-    const typeofDescription = options.typeof(description, defaultTypeof);
+// ####################
+// ## Implementation ##
+// ####################
 
-    switch (typeofDescription) {
+/**
+ * Maps a client object, `data`, into a sejr object.  `data` is not modified.
+ */
+function describe(data, options) {
+    const dataClientTypeName = options.typeof(data, defaultTypeof);
+    const description = buildDescription(dataClientTypeName, data, options);
+    return toSejr(dataClientTypeName, description);
+}
+
+/**
+ * Takes a client-type and a compatible description and returns a sejr object
+ * that will realize into the described value of the assumed type.
+ *
+ * Does not modify any of its parameters.
+ */
+ function toSejr(dataClientTypeName, untaggedDescription) {
+    let result;
+    let descriptionJsType = typeof untaggedDescription;
+    switch (descriptionJsType) {
         case 'boolean':
         case 'number':
         case 'string': {
-            if (typeof description !== typeofDescription) {
+            if (descriptionJsType === dataClientTypeName) {
+                result = untaggedDescription;
+            }
+            else {
+                result = metaNode(dataClientTypeName, untaggedDescription);
+            }
+            break;
+        }
+        case 'object': {
+            if (dataClientTypeName === 'object') {
+                if (untaggedDescription === null) {
+                    result = null;
+                }
+                else {
+                    let descriptionKeys = Object.keys(untaggedDescription);
+                    if (descriptionKeys.length === 1
+                            && descriptionKeys[0] === '@m') {
+                        result = metaNode(undefined, untaggedDescription);
+                    }
+                    else {
+                        result = untaggedDescription;
+                    }
+                }
+            }
+            else if (dataClientTypeName === 'array') {
+                result = untaggedDescription;
+            }
+            else {
+                result = metaNode(dataClientTypeName, untaggedDescription);
+            }
+            break;
+        }
+        default: {
+            throw new Error(`Description must be a JSON-representable value, ` +
+                    `but got value "${untaggedDescription}" of type `+
+                    `"${descriptionJsType}".`);
+        }
+    }
+    
+    return result;
+}
+
+/**
+ * Assumes that `data` is of client-type `dataClientTypeName` and builds a
+ * corresponding description (i.e., the `d` part of a hypothetical meta-node).
+ *
+ * Does not modify any of its parameters.
+ */
+function buildDescription(dataClientTypeName, data, options) {
+    const dataClientTypeDef = getTypeDef(options, dataClientTypeName);
+    let description = dataClientTypeDef.describe(data);
+    
+    const descriptionClientType = options.typeof(description, defaultTypeof);
+    switch (descriptionClientType) {
+        case 'boolean':
+        case 'number':
+        case 'string': {
+            if (typeof description !== descriptionClientType) {
                 throw new Error(`Custom typer reported type ` +
-                        `"${typeofDescription}", but value ` +
+                        `"${descriptionClientType}", but value ` +
                         `"${description}" has type ` +
                         `"${typeof description}".`);
             }
@@ -116,187 +202,204 @@ function describe(data, options) {
         }
     }
     
+    return description;
+}
+
+/**
+ * Maps a sejr object to a client object.
+ *
+ * Does not modify its parameters.
+ */
+function realize(sejrObject, options) {
+    let clientTypeName;
+    let description;
+
+    if (isMetaNode(sejrObject)) {
+        // Meta-nodes explicitly indicate their client type.
+    
+        const internals = sejrObject['@m'];
+        clientTypeName = internals.t || 'object';
+        description = internals.d;
+    }
+    else {
+        // Non-meta-nodes have an implied client type based on their structure
+        // and javascript type.
+    
+        description = sejrObject;
+        if (Array.isArray(sejrObject)) {
+            clientTypeName = 'array';
+        }
+        else {
+            clientTypeName = typeof sejrObject;
+        }
+    }
+    
+    return realizeDescription(clientTypeName, description, options);
+}
+
+/**
+ * Interprets the given `description` as the description of a value of
+ * client-type `typeName`, returning the result.
+ *
+ * Does not modify its parameters.
+ */
+function realizeDescription(typeName, description, options) {
     let result;
-    let primitiveTypeofDescription = typeof description;
-    switch (primitiveTypeofDescription) {
-        case 'boolean':
-        case 'number':
+    
+    if (isBuiltInClientType(typeName)) {
+        // Built-in client types provide a base case by interpretting at least
+        // one level of the description's structure.
+        result = realizeBuiltInClientTypeDescription(
+                typeName, description, options);
+    }
+    else {
+        // Custom client types fully defer to their type definitions to
+        // understand their descriptions.
+        result = realizeCustomClientTypeDescription(
+                typeName, description, options);
+    }
+    
+    return result;
+}
+
+/**
+ * Interprets the given `description` as the description of a value of
+ * custom client-type `typeName`, returning the result.
+ *
+ * Does not modify its parameters.
+ */
+function realizeCustomClientTypeDescription(typeName, description, options) {
+    let result;
+
+    const realizers = getTypeDef(options, typeName).realize;
+
+    const descriptionJsType = typeof description;
+    switch (descriptionJsType) {
+        case 'boolean': {
+            result = realizers.fromBoolean(description);
+            break;
+        }
+        case 'number': {
+            result = realizers.fromNumber(description);
+            break;
+        }
         case 'string': {
-            if (primitiveTypeofDescription === typeofData) {
-                result = description;
-            }
-            else {
-                result = { '@m': { t: typeofData, d: description }};
-            }
+            result = realizers.fromString(description);
+            break;
+        }
+        case 'undefined': {
+            result = realizers.fromUndefined();
             break;
         }
         case 'object': {
-            if (typeofData === 'object') {
-                if (description === null) {
-                    result = null;
-                }
-                else {
-                    let descriptionKeys = Object.keys(description);
-                    if (descriptionKeys.length === 1
-                            && descriptionKeys[0] === '@m') {
-                        result = { '@m': { d: description }};
-                    }
-                    else {
-                        result = description;
-                    }
-                }
+            let realizedDescription = realize(description, options);
+        
+            if (realizedDescription === null) {
+                result = realizers.fromNull(null);
             }
-            else if (typeofData === 'array') {
-                result = description;
+            else if (Array.isArray(realizedDescription)) {
+                result = realizers.fromArray(realizedDescription);
             }
             else {
-                result = { '@m': { t: typeofData, d: description }};
+                result = realizers.fromObject(realizedDescription);
             }
             break;
         }
         default: {
-            throw new Error(`Description must be a JSON-representable value, ` +
-                    `but got value "${description}" of type `+
-                    `"${primitiveTypeofDescription}".`);
+            throw new Error(`Description nested in meta node must be ` +
+                    `absent or a JSON-compatible type, but found value ` +
+                    `"${description}" of type "${descriptionJsType}".`);
         }
     }
     
     return result;
 }
 
-function realize(description, options) {
-    const typeofDescription = typeof description;
+/**
+ * Interprets the given `description` as the description of a value of
+ * built-in client-type `typeName`, returning the result.
+ *
+ * Does not modify its parameters.
+ */
+function realizeBuiltInClientTypeDescription(typeName, description, options) {
     let result;
     
-    switch (typeofDescription) {
+    switch (typeName) {
         case 'boolean':
         case 'number':
         case 'string': {
+            if (typeof description !== typeName) {
+                throw new Error('Shouldn\'t be possible.');
+            }
+            
             result = description;
             break;
         }
+        case 'array': {
+            if (!Array.isArray(description)) {
+                throw new Error('Shouldn\'t be possible.');
+            }
+        
+            result = new Array(description.length);
+            for (let i = 0; i < description.length; i++) {
+                result[i] = realize(description[i], options);
+            }
+            
+            break;
+        }
         case 'object': {
+            if (typeof description !== 'object') {
+                throw new Error('Shouldn\'t be possible.');
+            }
+        
             if (description === null) {
                 result = null;
             }
-            else if (Array.isArray(description)) {
-                result = new Array(description.length);
-                for (let i = 0; i < description.length; i++) {
-                    result[i] = realize(description[i], options);
-                }
-            }
             else {
-                const descriptionKeys = Object.keys(description);
-                if (descriptionKeys.length === 1 &&
-                        descriptionKeys[0] === '@m') {
-                    const escapedDescription = description['@m'];
-                    const describedType = escapedDescription.t || 'object';
-                    const typeDef = options.types[describedType];
-                    
-                    if (!typeDef) {
-                        throw new Error(`No type definition provided for ` +
-                                `type: "${describedType}".`);
-                    }
-                    
-                    const realizers = typeDef.realize;
-                    
-                    const nestedDescription = escapedDescription.d;
-                    const primitiveTypeofDesc = typeof nestedDescription;
-                    switch (primitiveTypeofDesc) {
-                        case 'boolean': {
-                            result = realizers.fromBoolean(nestedDescription);
-                            break;
-                        }
-                        case 'number': {
-                            result = realizers.fromNumber(nestedDescription);
-                            break;
-                        }
-                        case 'string': {
-                            result = realizers.fromString(nestedDescription);
-                            break;
-                        }
-                        case 'undefined': {
-                            result = realizers.fromUndefined();
-                            break;
-                        }
-                        case 'object': {
-                            let realizedNestedDescription;
-                            if (describedType === 'object') {
-                                realizedNestedDescription = realizeObject(
-                                        nestedDescription, options);
-                            }
-                            else {
-                                realizedNestedDescription =
-                                        realize(nestedDescription, options);
-                            }
-                        
-                            if (realizedNestedDescription === null) {
-                                result = realizers.fromNull(null);
-                            }
-                            else if (Array.isArray(nestedDescription)) {
-                                result = realizers.fromArray(
-                                        realizedNestedDescription);
-                            }
-                            else {
-                                result = realizers.fromObject(
-                                        realizedNestedDescription);
-                            }
-                            break;
-                        }
-                        default: {
-                            throw new Error(`Description nested in meta node ` +
-                                    `must be absent or a JSON-compatible ` +
-                                    `type, but found value ` +
-                                    `"${nestedDescription}" of type ` +
-                                    `"${primitiveTypeofDesc}".`);
-                        }
-                    }
-                }
-                else {
-                    result = realizeObject(description, options);
-                }
+                result = {};
+                Object.keys(description).forEach(key => {
+                    result[key] = realize(description[key], options);
+                });
             }
+            
             break;
         }
+        default: {
+            throw new Error('Not a built in client type: ' + typeName);
+        }
     }
     
     return result;
 }
 
-function realizeObject(input, options) {
-    if (typeof input !== 'object') {
-        throw new Error();
-    }
+function getTypeDef(options, typeName) {
+    const result = options.types[typeName];
     
-    let result;
-    if (input === null) {
-        result = null;
-    }
-    else {
-        result = {};
-        Object.keys(input).forEach(key => {
-            result[key] = realize(input[key], options);
-        });
+    if (!result) {
+        throw new Error(`No type definition provided for type: ${typeName}.`);
     }
     
     return result;
 }
 
-function defaultTypeof(d) {
-    const typeofD = typeof d;
-    
-    let result;
-    if (typeofD === 'object') {
-        if (Array.isArray(d)) {
-            result = 'array';
-        }
-        else {
-            result = 'object';
-        }
-    }
-    else {
-        result = typeofD;
+function metaNode(type, description) {
+    let internal = {};
+    if (typeof type !== 'undefined') {
+        internal.t = type;
     }
     
-    return result;
+    if (typeof description !== 'undefined') {
+        internal.d = description;
+    }
+    
+    return { '@m': internal };
+}
+
+function isMetaNode(n) {
+    return n !== null && typeof n === 'object' && Object.keys(n).length === 1 &&
+        typeof n['@m'] !== 'undefined';
+}
+
+function isBuiltInClientType(name) {
+    return !!builtInClientTypes[name];
 }
